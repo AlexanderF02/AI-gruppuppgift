4b1c1f978640d6dabcff4861d94524dcb2219b2c
Emil Eriksson <erikssonemil6080@gmail.com>
2025-01-22 14:42:05 +0100

    added daisyUI config and added Header comp

---
+++ components/header.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <div className="navbar bg-base-100">
+      <Link className="btn btn-ghost text-xl" href="/">
+        Home
+      </Link>
+      <Link className="btn btn-ghost text-xl" href="/chatbot">
+        Chatbot
+      </Link>
+      <Link className="btn btn-ghost text-xl" href="/imageAI">
+        Imgage recognition
+      </Link>
+    </div>
+  );
+}
