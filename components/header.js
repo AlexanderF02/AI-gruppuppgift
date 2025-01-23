850050384a3c83d56c0095878e27a7832e567531
joeljarvi <joeljarvi@live.se>
2025-01-23 11:42:55 +0100

    Nya grejer, daisy-theme bl.a

--- components/header.js
+++ components/header.js
@@ -1,23 +1,33 @@
 import Link from "next/link";
+import DarkModeSwitch from "./DarkModeSwitch";
 
 export default function Header() {
   return (
-    <div className="navbar bg-base-100">
-      <Link className="btn btn-ghost text-xl" href="/">
-        Home
-      </Link>
-      <Link className="btn btn-ghost text-xl" href="/chatbot">
-        Chatbot
-      </Link>
-      <Link className="btn btn-ghost text-xl" href="/imageAI">
-        Imgage recognition
-      </Link>
-      <Link className="btn btn-ghost text-xl" href="/ai-recommendations">
-        Recommendations
-      </Link>
-      <Link className="btn btn-ghost text-xl" href="/lady-content-ai">
-        LadyContent AI
-      </Link>
+    <div className="flex items-center justify-between navbar px-8 pt-4 pb-4 bg-neutral">
+      <div id="navContainer" classname="flex items-center">
+        <Link className="btn btn-ghost text-neutral-content" href="/">
+          Home
+        </Link>
+        <Link className="btn btn-ghost text-neutral-content" href="/chatbot">
+          Chatbot
+        </Link>
+        <Link className="btn btn-ghost text-neutral-content" href="/imageAI">
+          Imgage recognition
+        </Link>
+        <Link
+          className="btn btn-ghost text-neutral-content"
+          href="/ai-recommendations"
+        >
+          Recommendations
+        </Link>
+        <Link
+          className="btn btn-ghost text-neutral-content"
+          href="/lady-content-ai"
+        >
+          LadyContent AI
+        </Link>
+      </div>
+      <DarkModeSwitch />
     </div>
   );
 }
