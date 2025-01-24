b40e3a55d4cf9cdd381acabf919ff521a7819476
joeljarvi <joeljarvi@live.se>
2025-01-24 09:34:24 +0100

    klar med ai

--- components/header.js
+++ components/header.js
@@ -3,31 +3,33 @@ import DarkModeSwitch from "./DarkModeSwitch";
 
 export default function Header() {
   return (
-    <div className="flex items-center justify-between navbar px-8 pt-4 pb-4 bg-neutral">
-      <div id="navContainer" classname="flex items-center">
-        <Link className="btn btn-ghost text-neutral-content" href="/">
-          Home
-        </Link>
-        <Link className="btn btn-ghost text-neutral-content" href="/chatbot">
-          Chatbot
-        </Link>
-        <Link className="btn btn-ghost text-neutral-content" href="/imageAI">
-          Imgage recognition
-        </Link>
-        <Link
-          className="btn btn-ghost text-neutral-content"
-          href="/ai-recommendations"
-        >
-          Recommendations
-        </Link>
-        <Link
-          className="btn btn-ghost text-neutral-content"
-          href="/lady-content-ai"
-        >
-          LadyContent AI
-        </Link>
+    <span className="fixed left-0 top-0 w-screen">
+      <div className="flex items-center justify-between navbar px-8 pt-4 pb-4 bg-neutral">
+        <div id="navContainer" classname="flex items-center">
+          <Link className="btn btn-ghost text-neutral-content" href="/">
+            Home
+          </Link>
+          <Link className="btn btn-ghost text-neutral-content" href="/chatbot">
+            Chatbot
+          </Link>
+          <Link className="btn btn-ghost text-neutral-content" href="/imageAI">
+            Imgage recognition
+          </Link>
+          <Link
+            className="btn btn-ghost text-neutral-content"
+            href="/ai-recommendations"
+          >
+            Recommendations
+          </Link>
+          <Link
+            className="btn btn-ghost text-neutral-content"
+            href="/lady-content-ai"
+          >
+            LadyContent AI
+          </Link>
+        </div>
+        <DarkModeSwitch />
       </div>
-      <DarkModeSwitch />
-    </div>
+    </span>
   );
 }
