59d8deb4080a153d60a935350c0f8c08a1f91a89
Jenny Digné <jenny_digne@hotmail.com>
2025-01-25 17:04:07 +0100

    ändrat lite

--- pages/ai-recommendations.js
+++ pages/ai-recommendations.js
@@ -2,7 +2,7 @@ import { model } from "@/utils/ai";
 import { useState } from "react";
 
 const startPrompt = "Suggest 6 unique movie recommendations in this genre:  ";
-const endPrompt = "Give me the answer as a json including title, year of release and a short summary. Don't include the year in the title";
+const endPrompt = "Give me the answer as a json including title, year of release and a short summary. Don't include the year in the title. Don't recommend the same movies every time.";
 
 export default function AiRecommendations() {
   const [movies, setMovies] = useState([]);
@@ -23,7 +23,7 @@ export default function AiRecommendations() {
     <div className="min-h-screen">
       <div className="flex flex-col items-center">
         <h1 className="text-5xl font-bold mt-32 mb-8">Movie recommendations</h1>
-        <h2 className="text-xl font-medium mb-8">Pick a genre and get movie suggestions in that genre</h2>
+        <h2 className="text-xl font-medium mb-8">Get movie recommendations in the genre of your choice</h2>
         <select className="select select-bordered select-sm w-full max-w-xs mb-8" defaultValue={""} onChange={(e) => sendPrompt(e.target.value)}>
           <option value={""} disabled>Select a movie genre</option>
           <option value={"Comedy"}>Comedy</option>
@@ -35,9 +35,10 @@ export default function AiRecommendations() {
         </select>
       </div>
       <div>
-        <div className="flex flex-wrap justify-center gap-2 mb-8">
+      {movies.length ? <h2 className="text-center text-lg font-semibold mb-8">Here are your movie recommendations</h2> : <h2></h2>}
+        <div className="flex flex-wrap justify-center gap-4 mb-8">
           {movies.map((movie, index) => (
-            <div key={index} className="w-80 p-10">
+            <div key={index} className="w-80 p-10 border rounded-2xl">
               <p className="font-semibold mb-2">{movie.title} ({movie.year})</p>
               <p className="text-sm">{movie.summary}</p>
             </div>
