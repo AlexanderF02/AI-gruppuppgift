7d85f902817acf18635f7838738d5c3819603164
AlexanderF02 <alexander.fallstrom5@gmail.com>
2025-01-27 11:42:49 +0100

    fixat lite med chat bubble

--- pages/chatbot.js
+++ pages/chatbot.js
@@ -79,7 +79,7 @@ export default function AIChatbot() {
       </form>
 
       {showChat && (
-        <div className="w-full max-w-lg sm:max-w-xl p-8 rounded-lg shadow-md gap-2 mt-4">
+        <div className="w-full max-w-lg sm:max-w-xl p-8 gap-2 mt-4 bg-transparent">
           <div className="chat chat-start">
             <div className="chat-bubble">
               {message && message}
@@ -98,8 +98,3 @@ export default function AIChatbot() {
     </div>
   );
 }
-
-
-
-
-
